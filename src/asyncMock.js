3ebfc3ce8214385c0e63c3b993e02626204f8494
Sonia Pamela Juarez <spjuarez13591@gmail.com>
2022-07-24 22:49:01 -0300

    Contador con boton

---
+++ src/asyncMock.js
@@ -0,0 +1,13 @@
+const products = [
+    { 
+        id: '1', 
+        name: 'Pizza Napolitana', 
+        price: 100, 
+        category: 'Pizza', 
+        img:'https://placeimg.com/640/480/any', 
+        stock: 25, 
+        description:'Descripcion pizza napolitana'
+    },
+    { id: '2', name: 'Hamburguesa de queso', price: 150, category: 'hamburguesa', img:'https://placeimg.com/640/480/any', stock: 16, description:'Descripcion hamburguesa de queso'},
+    { id: '3', name: 'Ensalada caesar', price: 120, category: 'ensalada', img:'https://placeimg.com/640/480/any', stock: 10, description:'Descripcion ensalda caesar'}
+]
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,19 @@
 import './ItemListContainer.css';
+import Counter from '../ItemCount/ItemCount';
 
 const ItemListContainer = (props) => {
+
+    const handleOnAdd = (quantity) => {
+        alert(`Agregaste ${quantity} productos`);
+    }
     return (
-        <h1 className="item_greeting">{props.greeting}</h1>
+        <div>
+            <h1 className="item_greeting">{props.greeting}</h1>
+            <div className='card_container'>
+                <img className='card_img' src='images/pizza.png' alt='pizza'/>
+                <Counter initial={1} stock={10} onAdd={handleOnAdd}/>
+            </div>
+        </div>
     );
 }
 
