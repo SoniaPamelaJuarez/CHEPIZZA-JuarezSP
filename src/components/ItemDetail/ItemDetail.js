6aca878f0b9cb9429c1e7905b7bceefe5cf14730
Sonia Pamela Juarez <spjuarez13591@gmail.com>
2022-08-07 21:39:29 -0300

    Sincronizar Counter

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,11 +1,16 @@
 import '../Item/Item.css';
 import './ItemDetail.css';
 import Counter from '../ItemCount/ItemCount';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const ItemDetail = ({ name, img, price, stock, description_detalle }) => {
 
+    const [quantity, setQuantity] = useState (0);
+
     const handleOnAdd = (quantity) => {
-        alert(`Agregaste ${quantity} productos`);
+        console.log('Agregaste al carrito: ', quantity);
+        setQuantity(quantity)
     }
 
     return (
@@ -13,7 +18,7 @@ const ItemDetail = ({ name, img, price, stock, description_detalle }) => {
             <div className='card_container_item row'>
                 <img className='card_img_item' src={img} alt='pizza_img'/>
                 <div>
-                <Counter initial={1} stock={stock} onAdd={handleOnAdd}/>
+                {quantity > 0 ? <Link className='link_comprar' to='/cart'>Ir al carrito</Link> :<Counter initial={1} stock={stock} onAdd={handleOnAdd}/>}
                 <p className='letras_item'>{name} ${price}</p>
                 <p className='letras_item'>Stock disponible: {stock}</p>
                 <p className='letras_item'>{description_detalle}</p>
